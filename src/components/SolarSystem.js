fc0ab08bd8c619697b2ff2f31b82b9ac67dedc77
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-02-18 14:54:05 -0300

    Cumprimento do requisito 3

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
-    return <div data-testid="solar-system" />;
+    const name = 'sol';
+    return (
+      <div data-testid="solar-system">
+        <Title headline={ name } />
+      </div>
+    );
   }
 }
 
